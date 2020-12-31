880fd72b33743310df63a90031b4dc5482036ca6
h-shiraishi0402 <hiro19900402@gmail.com>
2021-01-01 03:04:19 +0900

    first commit

---
+++ DivingMemoriesApp/Controller/SearchView/SurroundingsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SurroundingsViewController.swift
+//  DivingMemoriesApp
+//
+//  Created by 白石裕幸 on 2020/12/19.
+//
+
+import UIKit
+
+class SurroundingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ DivingMemoriesApp/Model/ShopModel.swift
@@ -0,0 +1,8 @@
+//
+//  ShopModel.swift
+//  DivingMemoriesApp
+//
+//  Created by 白石裕幸 on 2020/11/08.
+//
+
+import Foundation
